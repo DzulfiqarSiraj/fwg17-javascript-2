6adb4a9697344168a68eb7df7cd7a89479a5f3dc
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2023-10-29 01:56:47 +0700

    Add files via upload

--- tugas3.js
+++ tugas3.js
@@ -2,20 +2,25 @@
 // Week 2
 // Oct 25, 2023
 
-const myArr = [2, 25, 4, 1, 30, 18]
-function sortedArray(arr){  
-  for(let i = 0; i < arr.length; i++){
-    for(let j = i; j < arr.length; j++){
-      if(arr[i] <= arr[j]){
-        arr[i] = arr[i]
+const myArr = [2, 25, 4, 1, 30, 18] 
+
+function sortedArray(arr){
+
+  let arrTemp = arr;
+
+  for(let i = 0; i < arrTemp.length; i++){
+    for(let j = i; j < arrTemp.length; j++){
+      if(arrTemp[i] <= arrTemp[j]){
+        arrTemp[i] = arrTemp[i]
       } else {
-        let num = arr[i]
-        arr[i] = arr[j]
-        arr[j] = num
+        let num = arrTemp[i]
+        arrTemp[i] = arrTemp[j]
+        arrTemp[j] = num
       }
     }
   }
-  return arr
+  
+  return arrTemp
 }
 
 function seleksiNilai(nilaiAwal, nilaiAkhir, arr){
@@ -44,7 +49,35 @@ function seleksiNilai(nilaiAwal, nilaiAkhir, arr){
 }
 
 
+// Menggunakan built in method
+function seleksiNilai2(nilaiAwal, nilaiAkhir, arr){
+  if(nilaiAwal > nilaiAkhir){
+    console.log("Nilai awal harus lebih kecil dari nilai akhir")
+    return
+  }
+  if(arr.length <= 5){
+    console.log("Jumlah data di dalam array harus lebih dari 5")
+    return
+  }
+
+  let arrNew = arr.sort((a,b) => a-b) // built-in method
+  let arrTemp = [];
+  for(let i = 0; i < arrNew.length; i++){
+    if(arrNew[i] > nilaiAwal && arrNew[i] < nilaiAkhir){
+      arrTemp.push(arr[i])
+    }
+  }
+  if(arrTemp.length === 0){
+    console.log("Nilai tidak ditemukan");
+    return
+  }
+
+  console.log(arrTemp)
+}
+
+
 seleksiNilai(5, 20 , [2, 25, 4, 14, 17, 30, 8])
+seleksiNilai2(5, 20 , [2, 25, 4, 14, 17, 30, 8])
 seleksiNilai(15, 3 , [2, 25, 4, 14, 17, 30, 8])
 seleksiNilai(4, 17 , [2, 25, 4])
 seleksiNilai(5, 17 , [2, 25, 4, 1, 30, 18])
